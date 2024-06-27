03b6e340cfd30e74199819d6654f82c4ab066c43
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2024-06-27 20:02:03 +0300

    Added Server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!")
+/** @format */
+
+const http = require("http")
+const path = require("path")
+const fs = require("fs")
+
+const server = http.createServer((req, res) => {
+	if (req.url == "/") {
+		res.end("<h1>Home</h1>")
+	}
+})
+
+const PORT = process.env.port || 5000
+
+server.listen(PORT, () => console.log(`Server not running on ${PORT}`))
